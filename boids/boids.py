4a50f4edb5aad87434a30a9a02ccf0d6a9c758fe
myx <yixuan0409@outlook.com>
2021-12-09 12:39:16 +0000

    replace numbers with varibles

--- boids/boids.py
+++ boids/boids.py
@@ -10,67 +10,37 @@ from matplotlib import animation
 
 import random
 
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
-boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
-boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
-
-# 
 class boid:
     boids_x = []
     boids_y = []
     boid_x_velocities = []
     boid_y_velocities = []
     
-    def _init_(low_x, low_y, up_x, up_y, x_v_low, y_v_low, x_v_up, y_v_up, number):
-        # self.boids_x = boids_x
-        # self.boids_y = boids_y
-        # self.boid_x_velocities = boid_x_velocities
-        # self.boid_y_velocities = boid_y_velocities
-
-        self.boids_x=[random.uniform(-450,50.0) for x in range(50)]
-        self.boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-        self.boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-        self.boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
+    def _init_(x_low, y_low, x_up, up_y, x_v_low, y_v_low, x_v_up, y_v_up, number):
+        
+        # initialize boids with given parameters
+        self.boids_x=[random.uniform(x_low,x_up) for x in range(number)]
+        self.boids_y=[random.uniform(y_low,y_up) for x in range(number)]
+        self.boid_x_velocities=[random.uniform(x_v_low,x_v_up) for x in range(number)]
+        self.boid_y_velocities=[random.uniform(y_v_low.0,y_v_up) for x in range(number)]
 
     def update_boids():
+        # for each boid
         for i in range(len(boids_x)):
             for j in range(len(boids_x)):
+                # Fly towards the middle
                 self.boid_x_velocities[i]=self.boid_x_velocities[i]+(self.boids_x[j]-self.boids_x[i])*0.01/len(self.boids_x)
                 self.boid_y_velocities[i]=self.boid_y_velocities[i]+(self.boids_y[j]-self.boids_y[i])*0.01/len(self.boids_x)
+                # Fly away from nearby boids
                 if (self.boids_x[j]-self.boids_x[i])**2 + (self.boids_y[j]-self.boids_y[i])**2 < 100:
                     self.boid_x_velocities[i]=self.boid_x_velocities[i]+(self.boids_x[i]-self.boids_x[j])
                     self.boid_y_velocities[i]=self.boid_y_velocities[i]+(self.boids_y[i]-self.boids_y[j])
+                # Try to match speed with nearby boids
                 if (boids_x[j]-boids_x[i])**2 + (boids_y[j]-boids_y[i])**2 < 10000:
                     self.boid_x_velocities[i]=self.boid_x_velocities[i]+(self.boid_x_velocities[j]-self.boid_x_velocities[i])*0.125/len(self.boids_x)
                     self.boid_y_velocities[i]=self.boid_y_velocities[i]+(self.boid_y_velocities[j]-self.boid_y_velocities[i])*0.125/len(self.boids_x)
 
-        # change the location of the boid
+        # Move according to velocities
         for i in range(len(boids_x)):
             boids_x[i]=self.boids_x[i]+self.boid_x_velocities[i]
             boids_y[i]=self.boids_y[i]+self.boid_y_velocities[i]
-
-
-# # update the location of the boid using its current location and velocities
-# def update_boids(boids):
-#     boids_x,boids_y,boid_x_velocities,boid_y_velocities=boids
-#     # Fly towards the middle
-#     # Fly away from nearby boids
-#     # Try to match speed with nearby boids
-#     # Move according to velocities
-#     for i in range(len(boids_x)):
-#         for j in range(len(boids_x)):
-#             boid_x_velocities[i]=boid_x_velocities[i]+(boids_x[j]-boids_x[i])*0.01/len(boids_x)
-#             boid_y_velocities[i]=boid_y_velocities[i]+(boids_y[j]-boids_y[i])*0.01/len(boids_x)
-#             if (boids_x[j]-boids_x[i])**2 + (boids_y[j]-boids_y[i])**2 < 100:
-#                 boid_x_velocities[i]=boid_x_velocities[i]+(boids_x[i]-boids_x[j])
-#                 boid_y_velocities[i]=boid_y_velocities[i]+(boids_y[i]-boids_y[j])
-#             if (boids_x[j]-boids_x[i])**2 + (boids_y[j]-boids_y[i])**2 < 10000:
-#                 boid_x_velocities[i]=boid_x_velocities[i]+(boid_x_velocities[j]-boid_x_velocities[i])*0.125/len(boids_x)
-#                 boid_y_velocities[i]=boid_y_velocities[i]+(boid_y_velocities[j]-boid_y_velocities[i])*0.125/len(boids_x)
-
-#     # change the location of the boid
-#     for i in range(len(boids_x)):
-#         boids_x[i]=boids_x[i]+boid_x_velocities[i]
-#         boids_y[i]=boids_y[i]+boid_y_velocities[i]
