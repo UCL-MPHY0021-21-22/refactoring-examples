45072f0f0daf432b2f3976e576a789abafe9504e
Kexuan Wang <512060578@qq.com>
2021-12-09 12:30:37 +0000

    Refactoring the codes

--- trees/trees.py
+++ trees/trees.py
@@ -2,16 +2,33 @@
 
 from math import sin, cos
 from matplotlib import pyplot as plt
-s=1
-d=[[0,1,0]]
+
+def CalculateNextNode(length, CurrentNode, NextNode):
+    NextNode.append([CurrentNode[j][0]+length*sin(CurrentNode[j][2]-0.1), CurrentNode[j][1]+length*cos(CurrentNode[j][2]-0.1), CurrentNode[j][2]-0.1])
+    NextNode.append([CurrentNode[j][0]+length*sin(CurrentNode[j][2]+0.1), CurrentNode[j][1]+length*cos(CurrentNode[j][2]+0.1), CurrentNode[j][2]+0.1])
+
+def DrawNodes(CurrentNode, NextNode):
+    plt.plot([CurrentNode[j][0], NextNode[-2][0]],[CurrentNode[j][1], NextNode[-2][1]])
+    plt.plot([CurrentNode[j][0], NextNode[-1][0]],[CurrentNode[j][1], NextNode[-1][1]])
+    
+length = 1
+
+CurrentNode = [[0,1,0]]
+
+# Draw the root of the tree
 plt.plot([0,0],[0,1])
-for i in range(5):
-    n=[]
-    for j in range(len(d)): #loop over d
-        n.append([d[j][0]+s*sin(d[j][2]-0.1), d[j][1]+s*cos(d[j][2]-0.1), d[j][2]-0.1])
-        n.append([d[j][0]+s*sin(d[j][2]+0.1), d[j][1]+s*cos(d[j][2]+0.1), d[j][2]+0.1])
-        plt.plot([d[j][0], n[-2][0]],[d[j][1], n[-2][1]])
-        plt.plot([d[j][0], n[-1][0]],[d[j][1], n[-1][1]])
-    d=n
-    s*=0.6
+
+BranchNumber = 5
+
+for i in range(BranchNumber):
+    
+    NextNode = []
+    
+    for j in range(len(CurrentNode)): #loop over d 
+        CalculateNextNode(length, CurrentNode, NextNode)
+        DrawNodes(CurrentNode, NextNode)
+        
+    CurrentNode = NextNode
+    length*=0.6
+    
 plt.savefig('tree.png')
